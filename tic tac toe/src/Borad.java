8009bd9f30070270ea28f9ee4396b66390510b61
tHEfLINGcAT <tamirbirbraer@gmail.com>
2023-12-25 22:33:15 +0200

    final (hopfully) commit

--- tic tac toe/src/Borad.java
+++ tic tac toe/src/Borad.java
@@ -1,64 +1,70 @@
 public class Borad {
-    public boolean win=false;
+    public boolean win = false;
     public char[][] borad;
-    Game g=new Game();
-    public Borad(){
-        borad=new char[3][3];
+
+    public Borad() {
+        borad = new char[3][3];
     }
-    public void definition(){
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                borad[i][j]='-';
-                System.out.print(borad[i][j]);
+
+    public void printBorad() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (borad[i][j] == 0) {
+                    System.out.print('-');
+                } else {
+                    System.out.print(borad[i][j]);
+                }
             }
             System.out.println();
         }
     }
-    public void endgame(){
-        if (borad[0][0]==borad[0][1]&&borad[0][1]==borad[0][2]) {
-            win=true;
-        }
-        if (borad[1][0]==borad[1][1]&&borad[1][1]==borad[1][2]) {
-            win=true;
-        }
-        if (borad[2][0]==borad[2][1]&&borad[2][1]==borad[2][2]) {
-            win=true;
+
+    public void endgame() {
+        if (borad[0][0] == borad[0][1] && borad[0][1] == borad[0][2]) {
+            win = true;
+        } else if (borad[1][0] == borad[1][1] && borad[1][1] == borad[1][2]) {
+            win = true;
+        } else if (borad[2][0] == borad[2][1] && borad[2][1] == borad[2][2]) {
+            win = true;
         }
 
-        if (borad[0][0]==borad[1][0]&&borad[1][0]==borad[2][0]){
-            win=true;
-        }    
-        if (borad[0][1]==borad[1][1]&&borad[1][1]==borad[2][1]) {
-            win=true;
-        }
-        if (borad[0][2]==borad[1][2]&&borad[1][2]==borad[2][2]) {
-            win=true;
+        else if (borad[0][0] == borad[1][0] && borad[1][0] == borad[2][0]) {
+            win = true;
+        } else if (borad[0][1] == borad[1][1] && borad[1][1] == borad[2][1]) {
+            win = true;
+        } else if (borad[0][2] == borad[1][2] && borad[1][2] == borad[2][2]) {
+            win = true;
         }
 
-        if (borad[0][0]==borad[1][1]&&borad[1][1]==borad[2][2]) {
-            win=true;
-        }
-        if (borad[0][2]==borad[1][1]&&borad[1][1]==borad[2][0]) {
-            win=true;
-        }
-        if (!(borad[0][0]==borad[0][1]&&borad[0][1]==borad[0][2])&&!(borad[1][0]==borad[1][1]&&borad[1][1]==borad[1][2])&&!(borad[2][0]==borad[2][1]&&borad[2][1]==borad[2][2])&&!(borad[0][0]==borad[1][0]&&borad[1][0]==borad[2][0])&&!(borad[0][1]==borad[1][1]&&borad[1][1]==borad[2][1])&&!(borad[0][2]==borad[1][2]&&borad[1][2]==borad[2][2])&&!(borad[0][0]==borad[1][1]&&borad[1][1]==borad[2][2])&&!(borad[0][2]==borad[1][1]&&borad[1][1]==borad[2][0])) {
-            win=true;
-            System.out.println("Its a draw");
-        }
-        else
-        {
-            win=false;
+        else if (borad[0][0] == borad[1][1] && borad[1][1] == borad[2][2]) {
+            win = true;
+        } else if (borad[0][2] == borad[1][1] && borad[1][1] == borad[2][0]) {
+            win = true;
         }
     }
+    public boolean drawCheck(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if (borad[i][j]==0) {
+                    return false;
+                }
+            }
+        }
+        return !win;
+    }
+
     public char[][] getBorad() {
         return borad;
     }
+
     public void setBorad(char[][] borad) {
         this.borad = borad;
     }
-    public boolean getWin(){
+
+    public boolean getWin() {
         return win;
     }
+
     public void setWin(boolean win) {
         this.win = win;
     }
--- tic tac toe/src/Game.java
+++ tic tac toe/src/Game.java
@@ -1,38 +1,48 @@
 import java.util.Scanner;
 
-public class Game{
-    Borad b=new Borad();
-    Players p =Players.inputPlayers();
-    private int x_row,x_columm;
-    public void innerLoop(){
-        Scanner s=new Scanner(System.in);
-            while (b.getWin()==false) {
+public class Game {
+    Borad b = new Borad();
+    Players p = Players.inputPlayers();
+    private int x_row, x_columm;
+
+    public void innerLoop() {
+        Scanner s = new Scanner(System.in);
+        while (b.getWin() == false || !b.drawCheck()) {
             System.out.println("player 1 turn:");
-            x_row=s.nextInt()-1;
-            x_columm=s.nextInt()-1;
-            b.borad[x_row][x_columm]='X';
-            System.out.println(b.borad);
-            System.out.println("player 2 turn:"); 
-            x_row=s.nextInt()-1;
-            x_columm=s.nextInt()-1;
-            b.borad[x_row][x_columm]='o';
-            System.out.println(b.borad);
-            if (b.borad[x_row][x_columm]=='x'||b.borad[x_row][x_columm]=='o') {
+            x_row = s.nextInt() - 1;
+            x_columm = s.nextInt() - 1;
+            while (b.borad[x_row][x_columm] == 'x' || b.borad[x_row][x_columm] == 'o') {
                 System.out.println("the cube is taken,make sure that the cube you are choosing isnt taken. ");
+                x_row = s.nextInt() - 1;
+                x_columm = s.nextInt() - 1;
             }
-            System.out.println(b.borad);
+            b.borad[x_row][x_columm] = 'x';
+            b.printBorad();
+            b.endgame();
+            if (b.getWin()==true) {
+                break;
+            }
+            System.out.println("player 2 turn:");
+            x_row = s.nextInt() - 1;
+            x_columm = s.nextInt() - 1;
+            while (b.borad[x_row][x_columm] == 'x' || b.borad[x_row][x_columm] == 'o') {
+                System.out.println("the cube is taken,make sure that the cube you are choosing isnt taken. ");
+                x_row = s.nextInt() - 1;
+                x_columm = s.nextInt() - 1;
+            }
+            b.borad[x_row][x_columm] = 'o';
+            b.printBorad();
+            b.endgame();
         }
     }
-    public void printWinner(){
-        if (b.getWin()==true) {
-            if (!(b.borad[0][0]==b.borad[0][1]&&b.borad[0][1]==b.borad[0][2])&&!(b.borad[1][0]==b.borad[1][1]&&b.borad[1][1]==b.borad[1][2])&&!(b.borad[2][0]==b.borad[2][1]&&b.borad[2][1]==b.borad[2][2])&&!(b.borad[0][0]==b.borad[1][0]&&b.borad[1][0]==b.borad[2][0])&&!(b.borad[0][1]==b.borad[1][1]&&b.borad[1][1]==b.borad[2][1])&&!(b.borad[0][2]==b.borad[1][2]&&b.borad[1][2]==b.borad[2][2])&&!(b.borad[0][0]==b.borad[1][1]&&b.borad[1][1]==b.borad[2][2])&&!(b.borad[0][2]==b.borad[1][1]&&b.borad[1][1]==b.borad[2][0])) {
-                if (b.borad[x_row][x_columm]=='x') {
-                    System.out.println(p.player_1+"has won!");
-                }
-            else if (b.borad[x_row][x_columm]=='y') {
-                System.out.println(p.player_2+"has won!");
-                }
-            }
+
+    public void printWinner() {
+        if (b.drawCheck()) {
+            System.out.println("its a draw!");
+        } else if (b.borad[x_row][x_columm] == 'x') {
+            System.out.println(p.player_1 + " has won!");
+        } else if (b.borad[x_row][x_columm] == 'o') {
+            System.out.println(p.player_2 + " has won!");
         }
     }
-} 
+}
